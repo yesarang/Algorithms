9c1dc29a78026f4e7e10a3d3234c7866d34f7d7b
iVehicle <anbox.support@gmail.com>
2014-04-24 15:29:41 +0900

    Added an loop-based binary tree in-order traversal algorithm

---
+++ src/tree_in_order.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct node {
+	int val_;
+	node* left_;
+	node* right_;
+	node(int val, node* left = nullptr, node* right = nullptr)
+		: val_(val), left_(left), right_(right)
+	{}
+};
+
+void print_in_order(node* root) {
+	stack<node*> s;
+	node* c = root;
+	s.push(c);
+	while (!s.empty()) {
+		c = s.top();
+		if (c->left_) {
+			s.push(c->left_);
+		}
+		else {
+			while (!s.empty()) {
+				c = s.top();
+				cout << c->val_ << " ";
+				s.pop();
+				if (c->right_) {
+					s.push(c->right_);
+					break;
+				}
+			}
+		}
+	}
+	cout << endl;
+}
+
+int main() {
+	node* tree = new node(1);
+	// 1
+	print_in_order(tree);
+
+	tree->left_ = new node(2);
+	// 2 1
+	print_in_order(tree);
+
+	tree->left_->left_ = new node(3);
+	// 3 2 1
+	print_in_order(tree);
+
+	tree->right_ = new node(4);
+	// 3 2 1 4
+	print_in_order(tree);
+
+	tree->right_->left_ = new node(5);
+	tree->right_->right_ = new node(6, nullptr, new node(7, 0, new node(8)));
+	// 3 2 1 5 4 6 7 8
+	print_in_order(tree);
+
+	return 0;
+}
