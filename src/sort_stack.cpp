4470b1a86934b331005fdb671b66e78bef27b349
iVehicle <anbox.support@gmail.com>
2014-05-07 20:10:27 +0900

    Enhanced sort_stack algorithm

--- src/sort_stack.cpp
+++ src/sort_stack.cpp
@@ -1,3 +1,7 @@
+// Write a program to sort a stack in ascending order(with biggest items on top).
+// You may use at most one additional stack to hold items, but you may not copy the
+// elements into any other data structure(such as an array).The stack supports the
+// following operations : push, pop, peek, and isEmpty.
 #include <iostream>
 #include <stack>
 
@@ -29,6 +33,32 @@ void sort_stack(stack<int>& s) {
 	}
 }
 
+void sort_stack2(stack<int>& s) {
+	stack<int> ts;
+	ts.push(s.top());
+	s.pop();
+	while (!s.empty()) {
+		int need_pop = 0;
+		int temp = s.top();
+		s.pop();
+		while (!ts.empty() && temp > ts.top()) {
+			s.push(ts.top());
+			ts.pop();
+			++need_pop;
+		}
+		ts.push(temp);
+		while (need_pop > 0) {
+			ts.push(s.top());
+			s.pop();
+			--need_pop;
+		}
+	}
+	while (!ts.empty()) {
+		s.push(ts.top());
+		ts.pop();
+	}
+}
+
 int main() {
 	stack<int> s;
 	for (auto i : { 7, 4, 5, -1, 9, 11 }) {
@@ -36,6 +66,15 @@ int main() {
 	}
 
 	sort_stack(s);
+	while (!s.empty()) {
+		cout << s.top() << endl;
+		s.pop();
+	}
+
+	for (auto i : { 7, 4, 5, -1, 9, 11 }) {
+		s.push(i);
+	}
+	sort_stack2(s);
 	while (!s.empty()) {
 		cout << s.top() << endl;
 		s.pop();
