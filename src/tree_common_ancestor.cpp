3d6884c2057b0d53593c1df9a800debf7c58d28a
iVehicle <anbox.support@gmail.com>
2014-05-14 06:15:10 -0700

    Added problem number

--- src/tree_common_ancestor.cpp
+++ src/tree_common_ancestor.cpp
@@ -1,4 +1,4 @@
-// Design an algorithm and write code to find the first common ancestor of two
+// 4.7 Design an algorithm and write code to find the first common ancestor of two
 // nodes in a binary tree. Avoid storing additional nodes in a data structure. NOTE:
 // This is not necessarily a binary search tree.
 #include <iostream>
